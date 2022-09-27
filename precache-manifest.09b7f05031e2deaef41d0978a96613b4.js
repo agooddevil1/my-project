69ba4e11068f2f718b030d99e99be3e5c5d6601c
agooddevil1 <lilgolu2000@gmail.com>
2022-09-27 13:51:30 +0530

    Updates

---
+++ precache-manifest.09b7f05031e2deaef41d0978a96613b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa52b5122cab261eef0",
+    "url": "/my-project/static/css/main.3843cdc4.chunk.css"
+  },
+  {
+    "revision": "2aa52b5122cab261eef0",
+    "url": "/my-project/static/js/main.2aa52b51.chunk.js"
+  },
+  {
+    "revision": "ea3f08b1317188f596e3",
+    "url": "/my-project/static/js/1.ea3f08b1.chunk.js"
+  },
+  {
+    "revision": "2617ddd4c220e075601a",
+    "url": "/my-project/static/js/runtime~main.2617ddd4.js"
+  },
+  {
+    "revision": "fe970ad61bd80a1cb7e839d71951281f",
+    "url": "/my-project/static/media/known.fe970ad6.jpeg"
+  },
+  {
+    "revision": "1871a1748a9b06510464013d9726fbca",
+    "url": "/my-project/static/media/nature.1871a174.jpg"
+  },
+  {
+    "revision": "80ccdaca03f33754bdf21a0c564bc9f5",
+    "url": "/my-project/static/media/nature2.80ccdaca.jpg"
+  },
+  {
+    "revision": "23e5bcb07316b2f43b01196cf277b6c1",
+    "url": "/my-project/static/media/nature3.23e5bcb0.jpg"
+  },
+  {
+    "revision": "b50540de45462d775c2dd8139114b9df",
+    "url": "/my-project/static/media/react1.b50540de.png"
+  },
+  {
+    "revision": "0da65edf50d09cf015243bbea0ef1952",
+    "url": "/my-project/static/media/react2.0da65edf.webp"
+  },
+  {
+    "revision": "02cd7fa2d20708866cdec1d5cba584f1",
+    "url": "/my-project/index.html"
+  }
+];
